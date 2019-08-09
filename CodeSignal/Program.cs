ef5316476e33c4dba022b0e303661aa16f5ef093
Jim Doyle <indellibleink@gmail.com>
2019-08-08 22:54:18 -0400

    added comments / instructions on usage

--- CodeSignal/Program.cs
+++ CodeSignal/Program.cs
@@ -6,7 +6,13 @@ namespace CodeSignal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Logon onto: https://codesignal.com to Company Challanges
+            // and cut & paste class's method into tests.
+            //
+            // Please read the code in the method to know what type of data
+            // is needed. The methods work in Visual Studio.
+            //
+            // Uncomment out the Day Class you want to execute.
         }
     }
 }
