1cf0a7c099b7527debff9b43d8ddbe83c81c4d75
Jim Doyle <indellibleink@gmail.com>
2019-09-25 22:52:00 -0400

    Completed Arcade Intro Five: shapeArea

--- CodeSignal/Arcade/Intro/Five_shapeArea.cs
+++ CodeSignal/Arcade/Intro/Five_shapeArea.cs
@@ -8,19 +8,20 @@ namespace CodeSignal.Arcade.Intro
     {
         public static void ShapeArea()
         {
-            int n = 4;
+            int n = 1;
             int count = 0;
+            int max = 2 * n - 1;
             int[] tally = new int[7];
-            // need to get odd numbers
-            if (n == 1)
-            {
-                Console.WriteLine(n);
-            }
-            for (int i=1; i<4; i++)
-            {
-                Console.WriteLine(i + i) ;
-            }
 
+
+            for (int i=1; i<n; i++)
+            {
+                Console.WriteLine(2*i-1);
+                int odd = 2 * i - 1;
+                count +=odd*2;
+            }
+            count += max;
+            Console.WriteLine(count);
         }
     }
 }
