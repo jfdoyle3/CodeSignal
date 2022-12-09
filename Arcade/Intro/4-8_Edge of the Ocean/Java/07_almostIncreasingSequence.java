448107ded697c2c3c2e3e9cac252c1df32540664
jfdoyle3 <indellibleink@gmail.com>
2022-12-09 13:19:40 -0500

    Still Working on almostIncreasingSequence

--- Arcade/Intro/4-8_Edge of the Ocean/Java/07_almostIncreasingSequence.java
+++ Arcade/Intro/4-8_Edge of the Ocean/Java/07_almostIncreasingSequence.java
@@ -1,22 +1,21 @@
 boolean solution(int[] sequence) {
-    boolean isSequenced=false;
+    boolean isSequenced=true;
     boolean isRemoved=false;
+    int countFalse=0;
     
-    /* Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence 	
-		by removing no more than one element from the array.
-		a0 < a1 < a2 < a3
-	*/
-	
+    // a0 < a1 < a2 < a3
+  
     for (int num=0; num<sequence.length-1; num++){
         if(sequence[num]<sequence[num+1])
             System.out.print("T"+num+ " ");
         else{
+            countFalse++;
             System.out.println("F"+num+" ");
         }
     }
-        
+    if (countFalse>=2)
+        return false;
 
 
     return isSequenced;
 }
-
